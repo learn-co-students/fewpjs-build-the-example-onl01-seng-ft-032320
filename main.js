784a1a684b9960da2639d7562b2721f804b2066f
ronniekram <arynn.boniface@gmail.com>
2020-06-30 14:14:54 -0700

    Done.

--- main.js
+++ main.js
@@ -1,16 +1,47 @@
 // Defining text characters for the empty and full hearts for you to use later.
+// Your JavaScript code goes here!
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const modal = document.getElementById('modal');
+let hearts = document.querySelectorAll(".like-glyph");
 
-// Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded",()=>{
+  for (let glyph of hearts) {
+    glyph.addEventListener("click", likeCallback);
+  }
+});
 
+function removeError() {
+  modal.classList.add('hidden');
+  modal.innerText = "";
+}
+
+function likeCallback(e) {
+  heart = e.target
+  mimicServerCall()
+  .then(function(e) {
+    if (heart.innerHTML === EMPTY_HEART) {
+      heart.innerHTML = FULL_HEART;
+      heart.classList.add('activated-heart');
+      console.log('post liked');
+    } else {
+      heart.innerHTML = EMPTY_HEART;
+      heart.classList.remove('activated-heart');
+      console.log('post unliked');
+    }
+  })
+  .catch(function(error) {
+    modal.classList.remove("hidden");
+    modal.innerText = error;
+    setTimeout(removeError, 5000)
+  });
+}
 
 
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
-
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
