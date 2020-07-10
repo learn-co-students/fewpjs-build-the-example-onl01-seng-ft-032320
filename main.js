3410caf82582163dc17adbe10fc689550c4ed272
Christopher <reinkendigital@gmail.com>
2020-07-09 20:49:26 -0500

    Done.

--- main.js
+++ main.js
@@ -3,8 +3,34 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const hearts = document.getElementsByClassName("like")
+let error = document.getElementById("modal");
+error.classList.add("hidden")
 
+for (const like of hearts) {
+  like.addEventListener("click", function() {
+    if (this.lastChild.innerText == EMPTY_HEART) {
+      let serverResponse = mimicServerCall();
+      console.log(serverResponse);
+      serverResponse.then(function() {
+        like.lastChild.innerText = FULL_HEART;
+        like.lastChild.classList.add("activated-heart");
+      })
+      .catch(function(data) {
+        error.classList.remove("hidden");
+        error.children[1].innerText = data;
 
+        setTimeout(function() {
+          error.classList.add("hidden");
+          error.children[1].innerText = "";
+        }, 5000)
+      });
+    } else {
+      this.lastChild.innerText = EMPTY_HEART;
+      this.lastChild.classList.remove("activated-heart");
+    }
+  });
+}
 
 
 //------------------------------------------------------------------------------
